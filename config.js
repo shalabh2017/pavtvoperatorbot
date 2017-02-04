aa1e0d48cb4026d40e2e795c19af0e45fa5f1fcc
Shalabh <agarwalshalabh@gmail.com>
2017-02-04 22:54:54 +0530

    Initial Configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAAIdjMvsmawBABv0BVqUJJhXl8ijuAs45tY6bi9RuYEZAuBlEDqlCpwwm9XmXrZANZBG7cfJYFe9a5ZBZAXcvgWZBg1XADURyvZCINsxX505iNoxfO4h3AUZCCJSwFpuvIIeLC0AHPXO91w2nbrBHpqKMpBQzlTWsttsb9oFD7zo1QZDZD',
+    FB_VERIFY_TOKEN: 'my_bot_for_pavtv',
+    API_AI_CLIENT_ACCESS_TOKEN: '81204986af8542cab2f373065d941433',
     FB_APP_SECRET: '',
-    SERVER_URL: "",
+    SERVER_URL: "https://mysterious-oasis-29230.herokuapp.com/",
 };
